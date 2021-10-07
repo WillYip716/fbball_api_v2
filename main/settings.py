6c4b280c3a34aedd2568b54d0cfb770484b94cbd
WillYip716 <william.yip.716@gmail.com>
2021-10-06 20:32:14 -0700

    migrations and naming fixes

--- main/settings.py
+++ main/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'fbballapi.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'fbballapi.wsgi.application'
+WSGI_APPLICATION = 'main.wsgi.application'
 
 
 # Database
